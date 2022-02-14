a980b30c3233ea1b65d4176a8d15bc6b9d63a3cd
michaelmlcavalcanti <michaelmlcavalcanti@gmail.com>
2022-02-14 20:33:36 -0300

    Criando projeto.

---
+++ src/br/com/java/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.java;
+
+/**
+ * @author Michael
+ * Testando integração Eclipse - GitHub
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
