2963b8b4e3ff2dc7ba6b6d356544385bc4b96e9b
michaelmlcavalcanti <michaelmlcavalcanti@gmail.com>
2022-02-14 20:41:44 -0300

    Adicionando um hello world ao git.

---
+++ src/br/com/java/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.java;
+
+/**
+ * @author Michael
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub");	
+	}
+
+}
